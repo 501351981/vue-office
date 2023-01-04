b0b0e99288438185e8791cf501ba8f6e801815e4
liyulin <501351981@qq.com>
2023-01-04 15:03:40 +0800

    修改example打包

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  outputDir:'examples/dist',
   pages: {
     index: {
       entry: 'examples/main.js',
