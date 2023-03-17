39c34b15b41408293be7dfd520bb066ffdbdd736
liyulin <501351981@qq.com>
2023-03-17 10:21:39 +0800

    fix: excel 修复移动端预览弹出键盘问题

--- packages/excel/src/hack.js
+++ packages/excel/src/hack.js
@@ -2,7 +2,6 @@ export function readOnlyInput(ref){
     let root = ref.value;
     if(root){
         let nodes = root.querySelectorAll('input');
-        console.log(nodes);
         for(let node of nodes){
             node && !node.readOnly && (node.readOnly = true);
         }
